30b180cde5b9acfa08a5cffdb05a991cf619060a
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-03-03 00:51:59 +0500

    Feat: Remove from CART + DB connection file added

--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -38,9 +38,37 @@ export async function GET(request: NextRequest, {params}: {params: Params}) {
       }
     });
   }
+
+  type CartBody = {
+    productId: string;
+  }
   
-  export async function POST() {
-    return new Response('Thank you for posting to this handler', {
+  export async function POST(request: NextRequest, {params}: {params: Params}) {
+    const userID = params?.id;
+    const body: CartBody = await request.json();
+    const productId = body?.productId;
+
+    carts[userID] = carts[userID] ? carts[userID].concat(productId): [productId];
+    const cartProducts = carts[userID].map(id => products.find(pd => pd.id === id));
+
+
+    return new Response(JSON.stringify(cartProducts), {
       status: 200,
-    })
+    });
+  }
+  
+  export async function DELETE(request: NextRequest, {params}: {params: Params}) {
+    const userID = params?.id;
+    const body: CartBody = await request.json();
+    const productId = body?.productId;
+
+    carts[userID] = carts[userID] ? carts[userID].filter(pId => pId !== productId): [];
+
+
+    return new Response(JSON.stringify(carts[userID]), {
+      status: 202,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
   }
